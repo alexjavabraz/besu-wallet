77ade157dcd44e9e72d155a6ec369453057548d3
bjbraz <contato@bjbraz.com.br>
2026-07-07 15:38:58 -0300

    karma

---
+++ karma.conf.js
@@ -0,0 +1,31 @@
+module.exports = function (config) {
+  config.set({
+    basePath: '',
+    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    plugins: [
+      require('karma-jasmine'),
+      require('karma-chrome-launcher'),
+      require('karma-jasmine-html-reporter'),
+      require('karma-coverage'),
+    ],
+    client: {
+      jasmine: {},
+      clearContext: false,
+    },
+    coverageReporter: {
+      dir: require('path').join(__dirname, './coverage/besu-wallet'),
+      subdir: '.',
+      reporters: [{ type: 'html' }, { type: 'text-summary' }],
+    },
+    reporters: ['progress', 'kjhtml'],
+    browsers: ['ChromeHeadlessCI'],
+    customLaunchers: {
+      ChromeHeadlessCI: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox', '--disable-gpu', '--disable-dev-shm-usage'],
+      },
+    },
+    singleRun: false,
+    restartOnFileChange: true,
+  });
+};
